ac60ac85aa731e0abb409dc4f63478f38bc6d742
robLittiere <robinl.95@orange.fr>
2021-01-27 13:28:43 +0100

    added feature to add the movies's year release.

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("hi")
+fs = require('fs')
+
+
+let file = "JSON/movies.json"
+let new_file = 'JSON/movies.out.json'
+transform(file, new_file);
+
+
+function transform(file, new_file){
+    fs.readFile(file, {encoding: 'utf8'}, function (err, data){
+        if (err){
+            return console.error(err);
+        }
+        
+        //Parse all movies in json object
+        let json = JSON.parse(data)
+        for (let i = 0; i < json.length; i ++){
+    
+            //Get year from JSON's release date
+            //Convert seconds from 1970 to actual year
+            let year = new Date(json[i]["release_date"] * 1000).getFullYear();   
+            //Write year next to the title
+            (json[i]["title"] += ' (' + year + ')')
+    
+        }
+
+        //Write json objects in new file
+        //Convert JSON objects to beautiful string
+        let string = JSON.stringify(json, null, 4)
+        
+        fs.writeFile(new_file, string, function(err){
+            if(err) return console.error(err);
+        })
+
+
+    })
+}
+
+
+
+
+
