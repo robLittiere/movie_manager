02839ae415c9c327326063f02ceaa6c34f5b59a2
killian <killian.gay28@gmail.com>
2021-01-28 09:42:43 +0100

    add parameters to the transform fonction

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const transform = require('./transform.js');
 program.version('0.0.1');
 
 program 
-    .command("transform")
+    .command("transform <input> <output>")
     .alias("t")
     .description("add the release year of a film next to it title ")
-    .action(function(){
-        transform.transform("JSON/movies.json", "JSON/.movies.out.json");
+    .action(function(input, output){
+        transform.transform(input, output);
     })
 
 program.parse(process.argv);
