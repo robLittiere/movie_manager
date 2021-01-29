7c31fa9ff3e9b28c3f0c1307c63e91b17e125703
robLittiere <robinl.95@orange.fr>
2021-01-29 09:41:37 +0100

    Added fixes search_keyword feature

--- Functions/search_keyword.js
+++ Functions/search_keyword.js
@@ -26,45 +26,55 @@ function findMovie(file, keyword, genre){
                 }    
             }
         }
-        let movie_genre_description = [];
-        for (let i = 0; i < movie_par_genre.length; i++){
-            let string = movie_par_genre[i].overview;
-            let overview = string.split(" ")
-            for (let j = 0; j < overview.length; j++){
-                if (keyword == overview[j]){
-                    movie_genre_description.push(movie_par_genre[i])
+
+        if(movie_par_genre.length != 0){
+            let movie_genre_description = [];
+            for (let i = 0; i < movie_par_genre.length; i++){
+                let string = movie_par_genre[i].overview;
+                let overview = string.split(" ")
+                for (let j = 0; j < overview.length; j++){
+                    if (keyword == overview[j]){
+                        movie_genre_description.push(movie_par_genre[i])
+                    }
                 }
-            }
-        }    
+            }    
+            
+            if (movie_genre_description.length != 0){
 
-        
-        let most_recent_movie;
-        for (let i = 0; i < movie_genre_description.length; i++){
-            if (i == movie_genre_description.length - 1){
-                if(most_recent_movie < movie_genre_description[i].release_date){
-                    most_recent_movie = movie_genre_description[i]
-                    
+                let most_recent_movie;
+                for (let i = 0; i < movie_genre_description.length; i++){
+                    if (i == movie_genre_description.length - 1){
+                        if(most_recent_movie < movie_genre_description[i].release_date){
+                            most_recent_movie = movie_genre_description[i]
+                            
+                        }
+                    }
+
+                    else if (movie_genre_description[i].release_date > movie_genre_description[i + 1].release_date){
+                        most_recent_movie = movie_genre_description[i]
+            
+                    }
+
+                    else if (movie_genre_description[i].release_date == movie_genre_description[i + 1].release_date){
+                        most_recent_movie = movie_genre_description[i]
+                    }
+                    else{
+                        most_recent_movie = movie_genre_description[i + 1]
+                    }
                 }
+
+                console.log("The most recent %s movie containing the keyword %s is %s ", genre, keyword, most_recent_movie.title)
+                let endDate = Date.now();
+                console.log("It took me %s seconds to find it", (endDate - startDate) / 1000)
             }
+            else {
+                console.log("There are words in the %s movies description that match your keyword %s", genre, keyword)
+            }    
+        }  
 
-            else if (movie_genre_description[i].release_date > movie_genre_description[i + 1].release_date){
-                most_recent_movie = movie_genre_description[i]
-    
-            }
-
-            else if (movie_genre_description[i].release_date == movie_genre_description[i + 1].release_date){
-                most_recent_movie = movie_genre_description[i]
-            }
-            else{
-                most_recent_movie = movie_genre_description[i + 1]
-            }
-        }
-
-        console.log("The most recent %s movie containing the keyword %s is %s ", genre, keyword, most_recent_movie.title)
-        let endDate = Date.now();
-        console.log("It took me %s seconds to find it", (endDate - startDate) / 1000)
-
-
+        else {
+                console.log("There are no movies in the genre %s or you entered a genre that doesn't exist in our database", genre)
+        }         
     })
 }    
 
